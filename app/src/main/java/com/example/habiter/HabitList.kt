83fdc8f6c4ae4f64850ba8d4ee5ffa7c6a010ca9
kanha-jii <kanhasinghal0101@gmail.com>
2023-06-27 18:30:16 +0530

    project commit 1st

---
+++ app/src/main/java/com/example/habiter/HabitList.kt
@@ -0,0 +1,16 @@
+package com.example.habiter
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+
+class HabitList : Fragment(R.layout.fragment_habit_list) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/com/example/habiter/data/models/Habit.kt
@@ -0,0 +1,19 @@
+package com.example.habiter.data.models
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+
+
+@Parcelize
+@Entity(tableName = "habit_table")
+data class Habit (
+        @PrimaryKey(autoGenerate = true)
+        val ID:Int,
+        val habit_title:String,
+        val habit_description:String,
+        val habit_startTime:String,
+        val imageId:Int
+        ): Parcelable
